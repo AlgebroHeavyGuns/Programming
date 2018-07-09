f13e2c4ad25ef0f9a515154b9726b58282ab7667
Algebro <algebro96@correo.ugr.es>
2018-07-09 11:02:12 +0200

    First cpp added

---
+++ Basics/00_HelloWorld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>		//Nos permite utilizar 'cout' y 'cin', para salida y entrada de datos (respectivamente)
+
+using namespace std;	//Define el espacio de nombres std.
+
+int main(){				//Función principal de uso obligatorio. Nuestro programa comenzará ejecutándose por aquí.
+
+	cout << "Hello World!!!" << endl;	//Indicamos que queremos mostrar el mensaje  "Hello World!!!" y además, un salto de línea.
+
+	return 0;			//Con un 0 indicamos al sistema que se ha ejecutado nuestro programa correctamente.
+}
