880cc9c327ea6d925d2159ae3804d1c8922b7c14
Algebro <algebro96@correo.ugr.es>
2018-12-27 10:29:56 +0100

    First exercises

---
+++ DataStructures/01_VectorConStruct.cpp
@@ -0,0 +1,62 @@
+/*
+
+	(C)Algebro.
+
+
+
+*/
+#include <iostream>
+
+
+using namespace std;
+
+typedef unsigned int type;		//nos permite cambiar el tipo de forma uniforme.
+
+struct Vector{
+  type* vec;
+  unsigned int size;
+};
+
+
+void createVector(Vector& vector, unsigned int newsize){
+  vector.vec = new type[newsize];
+  vector.size = newsize;
+}
+
+void deleteVector(Vector& vector){
+  delete vector.vec;
+  vector.size = -1;
+}
+
+void fillVector(Vector& vector, const type value){
+  for(unsigned int i=0; i<vector.size;i++)
+    vector.vec[i]=value;
+}
+
+void showVector(const Vector& vector, ostream& out, const char separator = '\t'){
+  for(unsigned int i=0; i<vector.size;i++)
+    out << vector.vec[i] << '\t';
+  out << endl;
+}
+
+const type& maxInVector(const Vector& vector){
+  type* max = vector.vec;
+  for(unsigned int i=1;i<vector.size;i++)
+    if(*max < vector.vec[i])
+      max = vector.vec+i;   // = &vector.vec[i]
+  return *max;
+}
+
+
+int main(int argc, char** argv){
+  Vector v;
+  createVector(v,5);
+  cout << "Vector size : " << v.size << endl;
+  fillVector(v, 1);
+  v.vec[2]= 2;
+  showVector(v, cout);
+  cout << "Maximum : " << maxInVector(v) << endl;
+
+
+  return 0;
+}
